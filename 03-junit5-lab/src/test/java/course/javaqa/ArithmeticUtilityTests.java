c5eefc0fced8156c9eaee5b0a470a5e5836f2466
Trayan Iliev <office@iproduct.org>
2022-01-27 19:11:37 +0200

    suite demo with junit5

--- 03-junit5-lab/src/test/java/course/javaqa/ArithmeticUtilityTests.java
+++ 03-junit5-lab/src/test/java/course/javaqa/ArithmeticUtilityTests.java
@@ -2,6 +2,14 @@ package course.javaqa;
 
 import course.javaqa.calculator.CalculatorTest;
 import course.javaqa.gcd.GcdTest;
+import org.junit.platform.suite.api.IncludeClassNamePatterns;
+import org.junit.platform.suite.api.SelectPackages;
+import org.junit.platform.suite.api.Suite;
+import org.junit.platform.suite.api.SuiteDisplayName;
 
+@Suite
+@SuiteDisplayName("JUnit Platform Suite Demo")
+@SelectPackages("course.javaqa")
+@IncludeClassNamePatterns(".*Test")
 public class ArithmeticUtilityTests {
 }
