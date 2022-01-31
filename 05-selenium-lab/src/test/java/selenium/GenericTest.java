98d408c1302fe1ec6d8f25498b6f15c6e343476a
Trayan Iliev <office@iproduct.org>
2022-01-31 20:25:35 +0200

    webdriver manager generic test added

---
+++ 05-selenium-lab/src/test/java/selenium/GenericTest.java
@@ -0,0 +1,37 @@
+package selenium;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class GenericTest {
+
+    WebDriver driver;
+
+    @ParameterizedTest
+    @ValueSource(classes = { ChromeDriver.class, FirefoxDriver.class })
+    void test(Class<? extends WebDriver> webDriverClass) {
+        // Driver management and WebDriver instantiation
+        driver = WebDriverManager.getInstance(webDriverClass).create();
+
+        // Exercise
+        driver.get("https://bonigarcia.dev/selenium-webdriver-java/");
+        String title = driver.getTitle();
+
+        // Verify
+        assertThat(title).contains("Selenium WebDriver");
+
+        // Tear down webdriver
+        driver.quit();
+    }
+
+}
