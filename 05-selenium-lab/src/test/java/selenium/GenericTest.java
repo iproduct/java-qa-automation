85bfcd336a45d4b9b36da557f432561b3487609f
Trayan Iliev <office@iproduct.org>
2022-02-03 19:10:14 +0200

    location redirection tested

--- 05-selenium-lab/src/test/java/selenium/GenericTest.java
+++ 05-selenium-lab/src/test/java/selenium/GenericTest.java
@@ -7,15 +7,25 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
+import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+
+import java.time.Duration;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class GenericTest {
 
     WebDriver driver;
+
+    @AfterEach
+    void teardown() {
+        driver.quit();
+    }
 
     @ParameterizedTest
     @ValueSource(classes = { ChromeDriver.class, FirefoxDriver.class })
@@ -29,9 +39,40 @@ public class GenericTest {
 
         // Verify
         assertThat(title).contains("Selenium WebDriver");
-
-        // Tear down webdriver
-        driver.quit();
     }
+
+    @ParameterizedTest
+    @ValueSource(classes = { ChromeDriver.class, FirefoxDriver.class })
+    void whenHomePageLoaded_thenTitleArticlesListAndLocationArticles(Class<? extends WebDriver> webDriverClass) {
+        // Driver management and WebDriver instantiation
+        driver = WebDriverManager.getInstance(webDriverClass).create();
+
+        // Exercise
+        driver.get("http://localhost:8080/");
+        String title = driver.getTitle();
+        JavascriptExecutor jse = (JavascriptExecutor)driver;
+        String location = (String)jse.executeScript("return window.location.href;");
+
+        // Verify
+        assertThat(location).isEqualTo("http://localhost:8080/articles");
+        assertThat(title).isEqualTo("Articles List");
+    }
+
+    @ParameterizedTest
+    @ValueSource(classes = { ChromeDriver.class, FirefoxDriver.class })
+    void whenHomePageLoaded_thenH3isArticlesList(Class<? extends WebDriver> webDriverClass) {
+        // Driver management and WebDriver instantiation
+        driver = WebDriverManager.getInstance(webDriverClass).create();
+
+        // Exercise
+        driver.get("http://localhost:8080/");
+        String title = driver.getTitle();
+
+        // Verify
+        assertThat(title).contains("Selenium WebDriver");
+    }
+
+
+
 
 }
