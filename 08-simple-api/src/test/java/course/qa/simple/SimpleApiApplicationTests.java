ec5460a5152d671e5b5ff432d0b362d57bc781e0
Trayan Iliev <office@iproduct.org>
2022-02-10 20:36:43 +0200

    REST API POST Article test added

--- 08-simple-api/src/test/java/course/qa/simple/SimpleApiApplicationTests.java
+++ 08-simple-api/src/test/java/course/qa/simple/SimpleApiApplicationTests.java
@@ -1,5 +1,6 @@
 package course.qa.simple;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import course.qa.simple.dao.ArticleRepository;
 import course.qa.simple.model.Article;
 import lombok.extern.slf4j.Slf4j;
@@ -12,11 +13,14 @@ import org.springframework.test.web.servlet.MockMvc;
 
 import java.util.List;
 
+import static org.hamcrest.Matchers.containsString;
+import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.BDDMockito.given;
 import static org.mockito.BDDMockito.then;
 import static org.mockito.Mockito.times;
 import static org.springframework.http.MediaType.APPLICATION_JSON;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @SpringBootTest
@@ -28,6 +32,9 @@ class SimpleApiApplicationTests {
 
     @Autowired
     private MockMvc mockMvc;
+
+    @Autowired
+    ObjectMapper mapper;
 
     @Test
     void givenArticles_whenGetArticles_thenStatus200JsonArray() throws Exception {
@@ -47,6 +54,34 @@ class SimpleApiApplicationTests {
         then(mockArticleRepository).should(times(1)).findAll();
         then(mockArticleRepository).shouldHaveNoMoreInteractions();
     }
+
+    @Test
+    void givenNewArticle_whenPostArticle_thenStatus201JsonObject() throws Exception {
+        // setup
+        given(mockArticleRepository.insert(any(Article.class))).willReturn(RESULT_ARTICLE);
+
+        // execute
+        mockMvc.perform(post("/api/articles")
+                        .contentType(APPLICATION_JSON)
+                        .content(mapper.writeValueAsString(NEW_ARTICLE))
+                        .accept(APPLICATION_JSON))
+                .andExpect(status().isCreated()) // verify
+                .andExpect(header().string("location",
+                        containsString("/api/articles/" + RESULT_ARTICLE.getId())))
+                .andExpect(content().contentType(APPLICATION_JSON))
+                .andDo(result -> log.info(result.getResponse().getContentAsString()))
+                .andExpect(jsonPath("$.id").value(RESULT_ARTICLE.getId()))
+                .andExpect(jsonPath("$.title").value(RESULT_ARTICLE.getTitle()))
+                .andExpect(jsonPath("$.content").value(RESULT_ARTICLE.getContent()));
+
+        then(mockArticleRepository).should(times(1)).insert(any(Article.class));
+        then(mockArticleRepository).shouldHaveNoMoreInteractions();
+    }
+
+    private static final Article NEW_ARTICLE =
+            new Article(null, "New Article", "New article content ...");
+    private static final Article RESULT_ARTICLE =
+            new Article("620547983857936bb34b9a3a", "New Article", "New article content ...");
 
 
     private static final List<Article> SAMPLE_ARTICLES = List.of(
